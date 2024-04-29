acb4bc5e06387636848325592579547cab7f60e8
Hilligans <squarehead2003@gmail.com>
2024-04-29 00:37:09 -0600

    cahnges

--- CST/src/main/java/org/example/Main.java
+++ CST/src/main/java/org/example/Main.java
@@ -1,7 +1,21 @@
 package org.example;
 
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@SpringBootApplication
 public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+    @RequestMapping("/")
+    String home() {
+        return "Hello World!";
     }
+
+    public static void main(String[] args) {
+        SpringApplication.run(Main.class, args);
+    }
+
 }
