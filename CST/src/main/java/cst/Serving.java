d5b9c4efccb55772d95cba77b452babec2b930bf
Ruel Nathaniel Alarcon <ruelnalarcon@gmail.com>
2024-04-30 22:25:15 -0600

    Cleaned frontend * Added template-based frontend * Fixed modal overlay support on chromium browsers * Fixed challenges without slash in Serving.java

--- CST/src/main/java/cst/Serving.java
+++ CST/src/main/java/cst/Serving.java
@@ -15,52 +15,60 @@ public class Serving {
         scores = Main.readString("/fetcher.html");
         main = Main.readString("/main.html");
         mainjs = Main.readString("/main.js");
-        challenges = Main.readString("challenges.json");
+        challenges = Main.readString("/challenges.json");
     }
 
     public String scores;
+
     @RequestMapping(value = "/scores")
     public String scores() {
         return scores;
     }
 
     public String style;
+
     @RequestMapping(value = "/i_style.css")
     public String style() {
         return style;
     }
 
     public String style1;
+
     @RequestMapping(value = "/f_style.css")
     public String style1() {
         return style1;
     }
 
     public String style2;
+
     @RequestMapping(value = "/h_style.css")
     public String style2() {
         return style2;
     }
 
     public String login;
+
     @RequestMapping(value = "/digitized2024")
     public String login() {
         return login;
     }
 
     public String main;
+
     @RequestMapping(value = "/main")
     public String main() {
         return main;
     }
 
     public String mainjs;
+
     @RequestMapping(value = "/main.js")
     public String mainjs() {
         return mainjs;
     }
 
     public String challenges;
+
     @RequestMapping(value = "/challenges.json")
     public String challenges() {
         return challenges;
--- CST/src/main/resources/main.js
+++ CST/src/main/resources/main.js
@@ -1,50 +1,34 @@
-import MicroModal from 'micromodal';
+document.addEventListener('DOMContentLoaded', function () {
+	fetch('/challenges.json')
+		.then(response => response.json())
+		.then(data => {
+			const template = document.querySelector('#template');
+			const buttonGrid = document.querySelector('#buttonGrid');
+			let modalCounter = 1; // starting counter for modal IDs
 
+			data.forEach((challenge, index) => {
+				const clone = document.importNode(template.content, true);
+				const challengeButton = clone.querySelector('button');
+				const modal = clone.querySelector('.micromodal');
+				const modalContainer = clone.querySelector('.micromodal__container');
+				const form = clone.querySelector('form');
+				const challengeNameField = clone.querySelector('input[type="hidden"]');
+				const challengeTitle = clone.querySelector('h2');
+				const challengeDescription = clone.querySelector('p');
 
+				// assign unique IDs and attributes
+				challengeButton.innerText = challenge.c_name;
+				challengeButton.setAttribute('data-micromodal-trigger', 'modal-' + modalCounter);
+				modal.id = 'modal-' + modalCounter;
+				modalContainer.setAttribute('aria-labelledby', 'modal-' + modalCounter + '-title');
+				challengeTitle.innerText = challenge.c_name;
+				challengeNameField.value = challenge.c_name;
+				challengeDescription.innerText = challenge.description;
 
+				buttonGrid.appendChild(clone);
+				modalCounter++;
+			});
 
-
-
-
-// // add all the elements inside modal which you want to make focusable
-// const focusableElements =
-//     'button, [href], input, select, textarea, [tabindex]:not([tabindex="-1"])';
-// const modal = document.querySelector('#exampleModal'); // select the modal by it's id
-
-// const firstFocusableElement = modal.querySelectorAll(focusableElements)[0]; // get first element to be focused inside modal
-// const focusableContent = modal.querySelectorAll(focusableElements);
-// const lastFocusableElement = focusableContent[focusableContent.length - 1]; // get last element to be focused inside modal
-
-
-// document.addEventListener('keydown', function (e) {
-//     let isTabPressed = e.key === 'Tab' || e.keyCode === 9;
-
-//     if (!isTabPressed) {
-//         return;
-//     }
-
-//     if (e.shiftKey) { // if shift key pressed for shift + tab combination
-//         if (document.activeElement === firstFocusableElement) {
-//             lastFocusableElement.focus(); // add focus for the last focusable element
-//             e.preventDefault();
-//         }
-//     } else { // if tab key is pressed
-//         if (document.activeElement === lastFocusableElement) { // if focused has reached to last focusable element then focus first focusable element after pressing tab
-//             firstFocusableElement.focus(); // add focus for the first focusable element
-//             e.preventDefault();
-//         }
-//     }
-// });
-
-// firstFocusableElement.focus();
-
-
-// MicroModal.init({
-//     onShow: modal => console.info(`${modal.id} is shown`), // [1]
-//     onClose: modal => console.info(`${modal.id} is hidden`), // [2]
-//     disableScroll: true, // [6]
-//     disableFocus: false, // [7]
-//     awaitOpenAnimation: false, // [8]
-//     awaitCloseAnimation: false, // [9]
-//     debugMode: false // [10]
-// });
+			MicroModal.init();
+		});
+});
